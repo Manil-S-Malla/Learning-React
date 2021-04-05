4f97eb39ff7fd4085146faa72f1e8bc4a188ea18
Manil S. Malla <manilsmalla@gmail.com>
2021-04-05 21:24:32 +0545

    Use State methods.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,2 +1,26 @@
-console.log("Hello World!");
+import store from './store';
+
+const unSubscribe = store.subscribe( () => {
+    console.log(`Store changed: ${store.getState()}`);
+});
+
+//unSubscribe();
+
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description: "Bug wont add.",
+    }
+})
+
+unSubscribe();
+
+store.dispatch({
+    type: "bugRemoved",
+    payload: {
+        id: 1,
+    }
+})
+
+console.log(store.getState());
  
